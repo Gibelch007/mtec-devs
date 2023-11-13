2a9dc1401b14cbb3dc37d3f2791667412ee39d08
gibelch007 <giovanaferrodrigues@gmail.com>
2023-11-13 10:06:24 -0300

    Descrever as alterações realizadas no projeto

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,60 +1,75 @@
 using System.Net.Mail;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
-    public class AccountController : Controller
-    {
-        private readonly ILogger<AccountController> _logger;
-        private readonly SignInManager<IdentityUser>_signInManager;
-        private readonly UserManager<IdentityUser>_userManager;
-
-    public static object MailAdress { get; private set; }
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
     public AccountController(
-            ILogger<AccountController> logger,
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager )
-        {
-            _logger = logger;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager)
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
 
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM loginVM = new() {
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+        };
+        return View(loginVM);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
         {
-            LoginVM loginVM = new(){
-                UrlRetorno = returnUrl ?? Url.Content("~/")
-            };
-            return View(loginVM);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginVM login)
-        {
-            if (ModelState.IsValid)
+            // Verifico o login
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
             {
-                //Verifico o Login
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                {
+                    userName = user.UserName;
+                }
+            }
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+
+            if (result.Succeeded)
+            {
 
             }
-            return View(login);
-        }
 
-        
-        private static bool IsValidEmail(string email)
+        }
+        return View(login);
+    }
+
+
+    private static bool IsValidEmail(string email)
+    {
+        try
         {
-            try
-            {
-                MailAddress m = new(email);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
+            MailAddress m = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
+}
